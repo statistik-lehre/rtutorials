5e523336d5b4678c8dba56effa06bfde4602752d
elbe <zur@posteo.de>
2022-08-08 21:10:26 +0200

    fixed formatting errors, updated description and readme, added dataset + documentation

---
+++ R/publictr.R
@@ -0,0 +1,25 @@
+#' public transport
+#'
+#' Daten über die Nutzung von Bussen und Bahnen in Deutschland von 2004 bis 2022
+#'
+#' @author © Statistisches Bundesamt (Destatis), 2022 | Stand: 04.08.2022
+#'
+#' @format 6 Variablen und 456 Beobachtungen
+#'
+#'
+#' \describe{Anmerkungen
+#'  \item{Liniennahverkehr insgesamt: Unternehmensfahrten}
+#'  \item{Liniennahverkehr mit Eisenbahnen, Straßenbahnen und Omnibussen: Verkehrsmittelfahrten.}
+#'  \item{Linienfernverkehr mit Omnibussen: Nach der Liberalisierung des
+#'  Linienfernverkehrs mit Omnibussen zum 1. Januar 2013 werden die neugegründeten
+#'   Unternehmen aus rechtlichen Gründen erst ab dem 1. Quartal 2016 in die Quartalserhebung einbezogen.
+#'   Um Fehlinterpretationen zu vermeiden, wurden daher bis zum 4. Quartal 2015
+#'   keine Ergebnisse für den Omnibus-Linienfernverkehr nachgewiesen.}
+#'  }
+#'
+#' @references
+#' Tabelle 46181-00005
+#' \url{https://www-genesis.destatis.de/genesis//online?operation=table&code=46181-0005&bypass=true&levelindex=0&levelid=1659561334845#abreadcrumb}
+#'
+
+"publictr"
---
+++ README.md
@@ -0,0 +1,25 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# tutor
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+This R package provides interactive tutorials, and wants to help third
+semester engineering students get a better understanding of the R
+programming language.  
+The contents are german, as this is a project of a german university.
+
+Additionaly, there are some real-life datasets supplied with this
+package to achieve a hands-on approach of data science.
+
+## Installation
+
+You can install the development version of tutor from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("remotes")
+remotes::install_github("statistik-lehre/tutor")
+```
