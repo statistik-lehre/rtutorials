480e9d1f5e4aa61d3bd169369bd0a8ff7a033ff7
elbe <zur@posteo.de>
2022-08-08 21:12:16 +0200

    add

--- R/publictr.R
+++ R/publictr.R
@@ -1,25 +1,32 @@
 #' public transport
 #'
-#' Daten über die Nutzung von Bussen und Bahnen in Deutschland von 2004 bis 2022
+#' Daten des statistischen Bundesamts über die Nutzung von Bussen und Bahnen in Deutschland von 2004 bis 2022
 #'
 #' @author © Statistisches Bundesamt (Destatis), 2022 | Stand: 04.08.2022
 #'
 #' @format 6 Variablen und 456 Beobachtungen
+#' \itemize{
+#'  \item Jahr
+#'  \item Quartal
+#'  \item Typ (siehe Anmerkungen)
+#'  \item Personen (beförderte Personen)
+#'  \item Personenkilometer
+#'  \item ausgewertete Unternehmen
+#' }
 #'
+#' \strong{Anmerkungen zur Variable Typ}
 #'
-#' \describe{Anmerkungen
-#'  \item{Liniennahverkehr insgesamt: Unternehmensfahrten}
-#'  \item{Liniennahverkehr mit Eisenbahnen, Straßenbahnen und Omnibussen: Verkehrsmittelfahrten.}
-#'  \item{Linienfernverkehr mit Omnibussen: Nach der Liberalisierung des
-#'  Linienfernverkehrs mit Omnibussen zum 1. Januar 2013 werden die neugegründeten
-#'   Unternehmen aus rechtlichen Gründen erst ab dem 1. Quartal 2016 in die Quartalserhebung einbezogen.
-#'   Um Fehlinterpretationen zu vermeiden, wurden daher bis zum 4. Quartal 2015
-#'   keine Ergebnisse für den Omnibus-Linienfernverkehr nachgewiesen.}
-#'  }
+#' \describe{
+#' \item{Liniennahverkehr insgesamt:}{Unternehmensfahrten}
+#' \item{Liniennahverkehr mit Eisenbahnen, Straßenbahnen und Omnibussen:}{Verkehrsmittelfahrten}
+#' \item{Linienfernverkehr mit Omnibussen:}{Nach der Liberalisierung des Linienfernverkehrs mit Omnibussen zum 1. Januar 2013 werden die neugegründeten Unternehmen aus rechtlichen Gründen erst ab dem 1. Quartal 2016 in die Quartalserhebung einbezogen. Um Fehlinterpretationen zu vermeiden, wurden daher bis zum 4. Quartal 2015 keine Ergebnisse für den Omnibus-Linienfernverkehr nachgewiesen}
+#' }
+#'
+#' @docType data
 #'
 #' @references
-#' Tabelle 46181-00005
-#' \url{https://www-genesis.destatis.de/genesis//online?operation=table&code=46181-0005&bypass=true&levelindex=0&levelid=1659561334845#abreadcrumb}
+#'
+#' \href{https://www-genesis.destatis.de/genesis//online?operation=table&code=46181-0005&bypass=true&levelindex=0&levelid=1659561334845#abreadcrumb}{Tabelle 46181-00005}
 #'
 
 "publictr"
--- README.md
+++ README.md
@@ -1,25 +1,35 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# tutor
+# tutor: interactive statistics tutorials
 
 <!-- badges: start -->
 <!-- badges: end -->
 
-This R package provides interactive tutorials, and wants to help third
-semester engineering students get a better understanding of the R
-programming language.  
-The contents are german, as this is a project of a german university.
+This R package provides interactive statistics tutorials, aiming to
+learn statistics simultaneosly to the R programming language.  
+It is tailored for third semester environmental and civil engineering
+students of Kassel university in Germany, and therefore the content is
+in German.
 
-Additionaly, there are some real-life datasets supplied with this
-package to achieve a hands-on approach of data science.
+Another feature of this package is that it comes with real-life
+engineering datasets instead of the fictious data, that many R courses
+provide.  
+This wants to facilitate a hands-on approach to data science with R that
+is connected to real life and therefore preserving the joy of answering
+real life questions.
 
 ## Installation
 
-You can install the development version of tutor from
-[GitHub](https://github.com/) with:
+You can install the `tutor` package from [GitHub](https://github.com/)
+with the help of the `remotes` package, which you need to install first.
+Copy the following code into your R console and hit Enter.
 
 ``` r
-# install.packages("remotes")
+install.packages("remotes")
 remotes::install_github("statistik-lehre/tutor")
 ```
+
+In RStudio, you will see the tutorials pane on the top left. Click there
+to select from different tutorials and start them. Feedback and
+contributions are welcome!
