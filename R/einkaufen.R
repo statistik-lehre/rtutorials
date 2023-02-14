43a49df99a233a77d880fc2f744be85bf6c2d9e3
lukasbrue <zur@posteo.de>
2023-02-14 14:56:14 +0100

    Added new dataset and documentation

---
+++ R/einkaufen.R
@@ -0,0 +1,32 @@
+#' einkaufen
+#'
+#' Von Studierenden durchgeführte Befragungen von Kund_innen
+#' (hauptsächlich vor Bioläden in und um Kassel)
+#'
+#' @author Studierende des WiSe 22/23 in Kassel
+#'
+#' @format 9 Variablen mit 166 Beobachtungen
+#'
+#'
+#'- **Befragung**:
+#'  Initialien der Studierenden, die die Befragung durchgeführt haben
+#'- **ID**:
+#'  Interviewnummer für jede einzelne Befragung
+#'
+#'- **Ort**
+#'- **Bioladen**:
+#'  TRUE / FALSE, je nach dem ob der Laden ein Bioladen war
+#'- **Datum**: YYYY-MM-DD
+#'- **Uhrzeit**
+#'- **Essen**: Lieblingsessen
+#'- **Kochen**: Anzahl der Tage pro Woche, an denen selber gekocht wird
+#'- **Verkehrsmittel**: Wie sind Sie hier her gekommen?
+#'- **Weg**: Wie weit war ihr Weg hier her? (in km)
+#'
+#'- **Frequenz**: Wie oft kochen Sie selbst?
+#'- **Alter**
+#'
+#' @docType data
+#'
+
+"einkaufen"
