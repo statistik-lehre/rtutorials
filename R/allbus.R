27d4ee141d91dd025fd03d576f013851ce259f13
Lukas Bruelheide <zur@posteo.de>
2023-07-19 23:35:22 +0200

    Habe den Allbus 2021 Datensatz hinzugefügt

---
+++ R/allbus.R
@@ -0,0 +1,27 @@
+#' Allbus 2021
+#'
+#' Allgemeine Bevölkerungsumfrage der Sozialwissenschaften
+#'
+#'
+#'
+#' @source https://www.gesis.org/allbus/download
+#'
+#' @format 544 Variablen mit 5342 Beobachtungen
+#'
+#' Eine Liste aller Variablen kann in der `dictionary`-Tabelle nachgesehen werden
+#'
+#' Metrische Variablen:
+#'
+#'
+#'- **age**:
+#'  Alter
+#'- **di05**:
+#'  Haushaltsnettoeinkommen
+#'
+#'- **lm02**:
+#'  Fernsehgesamtdauer pro Tag in Minuten
+#'
+#'
+#' @docType data
+
+"allbus"
