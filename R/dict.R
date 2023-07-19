551cc947b3fe9d7e98614892e75052d8f72cffe4
Lukas Bruelheide <zur@posteo.de>
2023-07-19 23:49:27 +0200

    Hinzufügen eines Codebooks für Allbus-Datensatz

---
+++ R/dict.R
@@ -0,0 +1,43 @@
+#' Codebook zum `allbus`-Datensatz
+#'
+#' (dictionary)
+#'
+#' Daten: Allbus 2021
+#'
+#' Allgemeine Bevölkerungsumfrage der Sozialwissenschaften
+#'
+#'
+#'
+#'
+#' @format 7 Spalten und 6930 Zeilen
+#'
+#' Eine Liste aller Variablen und den Bezeichnungen ihrer Ausprägungen
+#'
+#'
+#' Das Label `[-32] NICHT GENERIERBAR` bezeichnet metrische Ausprägungen
+#'
+#' Die Liste ist im Langformat, d.h. zu einer Variable können mehrere Spalten
+#' gehören wenn sie mehrere Ausprägungen besitzt.
+#'
+#'
+#'- **pos**:
+#'  Position
+#'- **variable**:
+#'  in R nutzbarerer Name
+#'
+#'- **label**:
+#'  Titel
+#'
+#'- **col_type**:
+#'  Datentyp
+#'
+#'- **missing**:
+#'  Anzahl fehlender Werte
+#'
+#'- **value-labels**:
+#'  Codierungen
+#'
+#'
+#' @docType data
+
+"dict"
