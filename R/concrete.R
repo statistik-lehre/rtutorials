91dd0275cbeaca9ba839278d0addbc159f8941bf
Lukas Bruelheide <zur@posteo.de>
2023-07-21 15:08:08 +0200

    copyright hinzugefügt

--- R/concrete.R
+++ R/concrete.R
@@ -75,5 +75,15 @@
 #'
 #' @source https://archive.ics.uci.edu/dataset/165/concrete+compressive+strength
 #'
+#' DOI: 10.24432/C5PK67
+#' @details
+#' Acknowledgements, Copyright Information, and Availability:
+#'
+#' NOTE: Reuse of this database is unlimited with retention of copyright notice for
+#' Prof. I-Cheng Yeh and the following published paper:
+#'
+#'   I-Cheng Yeh, "Modeling of strength of high performance concrete using artificial
+#' neural networks," Cement and Concrete Research, Vol. 28, No. 12, pp. 1797-1808 (1998)
+#'
 #' @docType data
 "concrete"
