37c7c7070c151dc5610ad0ff8e4fc2b9c75786a6
Lukas Bruelheide <zur@posteo.de>
2023-10-31 12:24:53 +0100

    Dokumentation für cranpacks Datensatz

---
+++ R/cranpacks.R
@@ -0,0 +1,19 @@
+#' cranpacks
+#'
+#' Liste der auf CRAN veröffentlichten Pakete, Stand 25.10.2023
+#'
+#'
+#' @format 4 Variablen mit 19995 Beobachtungen
+#'
+#'
+#'- **id**: fortlaufende Nummer (zählt nur Pakete, die zum Stand veröffentlicht sind)
+#'- **date**: Datum der Aufnahme in CRAN
+#'- **package**: Paketname
+#'- **title**: Titel des Pakets
+#'
+#' @docType data
+#'
+#' @source abgerufen von https://cran.r-project.org/web/packages/available_packages_by_date.html
+#'
+
+"cranpacks"
