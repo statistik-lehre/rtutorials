3d9c578712011651c5df0a393698a63aa9893858
Lukas Bruelheide <zur@posteo.de>
2023-12-20 20:01:09 +0100

    einkaufen dataset: convert some variables to factors (#55)
    Also rearranged the columns marginally to have ort next to bioladen and put those more in the front

--- data-raw/einkaufen/einkaufen.R
+++ data-raw/einkaufen/einkaufen.R
@@ -7,10 +7,15 @@ einkaufen <- read_csv("data-raw/einkaufen/einkaufen.csv",
 # rearrange columns
 
 einkaufen <- einkaufen |>
-  select(Essen, Verkehrsmittel, Weg, Alter, Kochen, Frequenz, Befragung, ID, Ort,
-         Datum, Uhrzeit, Bioladen)
+  select(Essen, Verkehrsmittel, Weg, Alter, Kochen, Frequenz, Ort, Bioladen, Befragung, ID,
+         Datum, Uhrzeit)
 
 # convert column names to snake_case
 colnames(einkaufen) <- tolower(colnames(einkaufen))
+
+# convert some columns to factors
+
+einkaufen |>
+  mutate(across(c(essen, verkehrsmittel, frequenz, befragung, ort), forcats::as_factor))
 
 usethis::use_data(einkaufen, overwrite = TRUE, version = 3)
