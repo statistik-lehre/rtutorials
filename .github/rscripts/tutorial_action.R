a20c80b48260c4485c999aa20263ac553ce9e73a
Lukas Bruelheide <zur@posteo.de>
2024-01-16 23:23:30 +0100

    Add automatic Tutorial Render Check via GitHub Actions
    I am proud bc its my first GitHub Actions stuff and I got it to work

---
+++ .github/rscripts/tutorial_action.R
@@ -0,0 +1,22 @@
+# Build and install the local package for example data sets to work
+devtools::install()
+
+# 1. Install necessary dependencies
+deps <- renv::dependencies("./inst/tutorials")$Package |> unique()
+installed <- installed.packages()
+to_install <- deps[!(deps %in% installed)]
+lapply(to_install, install.packages)
+
+# 2. Get all possible tutorial paths
+
+tutorial_paths <- list.files("inst/tutorials", recursive = TRUE)
+tutorial_paths <- tutorial_paths[endsWith(tutorial_paths, ".Rmd")]
+tutorial_paths <- tutorial_paths[!is.na(tutorial_paths)]
+tutorial_paths <- paste0("inst/tutorials/", tutorial_paths)
+
+# 3. Render every tutorial
+
+for (p in tutorial_paths) {
+  rmarkdown::render(p)
+}
+
