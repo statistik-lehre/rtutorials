2a71fc6a45933d040d1f9c062afecf916487637d
Lukas Bruelheide <zur@posteo.de>
2024-01-16 23:39:28 +0100

    try to fix package install problems

--- .github/rscripts/tutorial_action.R
+++ .github/rscripts/tutorial_action.R
@@ -1,11 +1,11 @@
 # Build and install the local package for example data sets to work
-devtools::install()
+# devtools::install()
 
 # 1. Install necessary dependencies
 deps <- renv::dependencies("./inst/tutorials")$Package |> unique()
 installed <- installed.packages()
 to_install <- deps[!(deps %in% installed)]
-lapply(to_install, install.packages)
+lapply(to_install, pak::pkg_install)
 
 # 2. Get all possible tutorial paths
 
